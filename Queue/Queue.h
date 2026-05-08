7f910b49fbc3b71cfff213de93204ae2c0a76d87
Lia <122105453+CanadianPS1@users.noreply.github.com>
2026-05-08 14:56:16 -0600

    added comments

--- Queue/Queue.h
+++ Queue/Queue.h
@@ -6,14 +6,47 @@ template<typename T>
 class Queue{
     SingleLinkedList<T> queue;
     public:
+    /**
+     * @brief removes an item from the list
+     * 
+     * @return T 
+     */
     T Dequeue(){return queue.Remove();}
+    /**
+     * @brief looks at the first item
+     * 
+     * @return T 
+     */
     T Peak(){return queue.Get(0);}
+    /**
+     * @brief adds an item to the list
+     * 
+     * @param data 
+     */
     void Enqueue(T data){queue.Add(data);}
+    /**
+     * @brief grabs the value at an index in the list
+     * 
+     * @param index 
+     * @return T 
+     */
     T Get(int index){return queue.Get(index);}
+    /**
+     * @brief checks if the value is inside the list
+     * 
+     * @param value 
+     * @return true 
+     * @return false 
+     */
     bool Contains(int value){
         int index = queue.Search(value);
         if(index < 0) return false;
         else return true;
     }
+    /**
+     * @brief returns the size of the list
+     * 
+     * @return int 
+     */
     int Size(){return queue.Size();}
 };
--- Stack/Stack.h
+++ Stack/Stack.h
@@ -6,14 +6,47 @@ template<typename T>
 class Stack{
     DoubleLinkedList<T> list;
     public:
+    /**
+     * @brief removes an item from the list
+     * 
+     * @return T 
+     */
     T Pop(){return list.RemoveLast();}
+    /**
+     * @brief looks at the first item in the list
+     * 
+     * @return T 
+     */
     T Peak(){return list.Get(list.Size() - 1);}
+    /**
+     * @brief adds an item to the list
+     * 
+     * @param data 
+     */
     void Push(T data){list.Add(data);}
+    /**
+     * @brief grabs the value at an index
+     * 
+     * @param index 
+     * @return T 
+     */
     T Get(int index){return list.Get(index);}
+    /**
+     * @brief checks if the list contains an item
+     * 
+     * @param value 
+     * @return true 
+     * @return false 
+     */
     bool Contains(int value){
         int index = list.Search(value);
         if(index < 0) return false;
         else return true;
     }
+    /**
+     * @brief returns the size of the list
+     * 
+     * @return int 
+     */
     int Size(){return list.Size();}
 };
